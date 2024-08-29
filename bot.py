c9e006c688d5633d0cc6ad4553f83150ffd3f7cf
erik316 <144471001+erik316@users.noreply.github.com>
2024-08-29 18:12:14 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,73 +1,23 @@
 from telebot import TeleBot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-from logic import *
+from logic import DatabaseManager, hide_img
 import schedule
 import threading
 import time
-from config import *
+from config import API_TOKEN, DATABASE
 import logging
 
 bot = TeleBot(API_TOKEN)
+manager = DatabaseManager(DATABASE)
 
-def gen_markup(id):
+def gen_markup(prize_id):
     markup = InlineKeyboardMarkup()
     markup.row_width = 1
-    markup.add(InlineKeyboardButton("Получить!", callback_data=id))
+    markup.add(InlineKeyboardButton("Получить!", callback_data=prize_id))
     return markup
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-
-    prize_id = call.data
-    user_id = call.message.chat.id
-
-    img = manager.get_prize_img(prize_id)
-    with open(f'img/{img}', 'rb') as photo:
-        bot.send_photo(user_id, photo)
-
-
-def send_message():
-    prize_id, img = manager.get_random_prize()[:2]
-    manager.mark_prize_used(prize_id)
-    hide_img(img)
-    for user in manager.get_users():
-        with open(f'hidden_img/{img}', 'rb') as photo:
-            bot.send_photo(user, photo, reply_markup=gen_markup(id = prize_id))
-        
-
-def shedule_thread():
-    schedule.every(1).minute.do(send_message) # Здесь ты можешь задать периодичность отправки картинок
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    user_id = message.chat.id
-    if user_id in manager.get_users():
-        bot.reply_to(message, "Ты уже зарегестрирован!")
-    else:
-        manager.add_user(user_id, message.from_user.username)
-        bot.reply_to(message, """Привет! Добро пожаловать! 
-Тебя успешно зарегистрировали!
-Каждый час тебе будут приходить новые картинки и у тебя будет шанс их получить!
-Для этого нужно быстрее всех нажать на кнопку 'Получить!'
-
-Только три первых пользователя получат картинку!)""")
-        
-@bot.message_handler(commands=['rating'])
-def handle_rating(message):
-    res = manager.get_rating() 
-    res = [f'| @{x[0]:<11} | {x[1]:<11}|\n{"_"*26}' for x in res]
-    res = '\n'.join(res)
-    res = f'|USER_NAME    |COUNT_PRIZE|\n{"_"*26}\n' + res
-    bot.send_message(message.chat.id, res)
-    
-    
-    
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-
     prize_id = call.data
     user_id = call.message.chat.id
 
@@ -76,23 +26,78 @@ def callback_query(call):
         if res:
             img = manager.get_prize_img(prize_id)
             with open(f'img/{img}', 'rb') as photo:
-                bot.send_photo(user_id, photo, caption="Поздравляем! Ты получил картинку!")
+                bot.send_photo(user_id, photo, caption="Поздравляем! Ты получил картинку! Тебе начислено 10 баллов.")
         else:
             bot.send_message(user_id, 'Ты уже получил картинку!')
     else:
         bot.send_message(user_id, "К сожалению, ты не успел получить картинку! Попробуй в следующий раз!)")
 
+def send_message():
+    try:
+        prize_id, img = manager.get_random_prize()[:2]
+        manager.mark_prize_used(prize_id)
+        hide_img(img)
+
+        for user in manager.get_users():
+            with open(f'hidden_img/{img}', 'rb') as photo:
+                bot.send_photo(user, photo, reply_markup=gen_markup(prize_id))
+    except Exception as e:
+        logging.error(f"Error sending message: {e}")
+
+def schedule_thread():
+    schedule.every(1).minute.do(send_message)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    user_id = message.chat.id
+    if user_id in manager.get_users():
+        bot.reply_to(message, "Ты уже зарегистрирован!")
+    else:
+        manager.add_user(user_id, message.from_user.username)
+        bot.reply_to(message, """Привет! Добро пожаловать! 
+Тебя успешно зарегистрировали!
+Каждый час тебе будут приходить новые картинки, и у тебя будет шанс их получить!
+Для этого нужно быстрее всех нажать на кнопку 'Получить!'
+
+Только три первых пользователя получат картинку!)""")
+
+@bot.message_handler(commands=['rating'])
+def handle_rating(message):
+    res = manager.get_rating()
+    res = [f'| @{x[0]:<11} | {x[1]:<11}|\n{"_"*26}' for x in res]
+    res = '\n'.join(res)
+    res = f'|USER_NAME    |COUNT_PRIZE|\n{"_"*26}\n' + res
+    bot.send_message(message.chat.id, res)
+
+@bot.message_handler(commands=['points'])
+def handle_points(message):
+    user_id = message.chat.id
+    points = manager.get_points(user_id)
+    bot.send_message(user_id, f"У тебя {points} баллов.")
+
+@bot.message_handler(commands=['redeem'])
+def handle_redeem(message):
+    user_id = message.chat.id
+    args = message.text.split()
+    if len(args) == 2 and args[1].isdigit():
+        points = int(args[1])
+        if manager.redeem_points(user_id, points):
+            bot.send_message(user_id, f"Ты успешно обменял {points} баллов на бонус!")
+            # Implement the actual bonus awarding logic here
+        else:
+            bot.send_message(user_id, f"У тебя недостаточно баллов для обмена. У тебя {manager.get_points(user_id)} баллов.")
+    else:
+        bot.send_message(user_id, "Пожалуйста, используй формат: /redeem <количество_баллов>")
 
 def polling_thread():
     bot.polling(none_stop=True)
 
 if __name__ == '__main__':
-    manager = DatabaseManager(DATABASE)
-    manager.create_tables()
-
     polling_threadX = threading.Thread(target=polling_thread)
-    polling_shedule  = threading.Thread(target=shedule_thread)
+    polling_schedule = threading.Thread(target=schedule_thread)
 
     polling_threadX.start()
-    polling_shedule.start()
-  
+    polling_schedule.start()
--- logic.py
+++ logic.py
@@ -1,12 +1,14 @@
 import sqlite3
-from datetime import datetime
-from config import DATABASE 
 import os
 import cv2
+import numpy as np
+from datetime import datetime
+from config import DATABASE
 
 class DatabaseManager:
     def __init__(self, database):
         self.database = database
+        self.create_tables()
 
     def create_tables(self):
         conn = sqlite3.connect(self.database)
@@ -14,9 +16,10 @@ class DatabaseManager:
             conn.execute('''
             CREATE TABLE IF NOT EXISTS users (
                 user_id INTEGER PRIMARY KEY,
-                user_name TEXT
+                user_name TEXT,
+                points INTEGER DEFAULT 0
             )
-        ''')
+            ''')
 
             conn.execute('''
             CREATE TABLE IF NOT EXISTS prizes (
@@ -24,7 +27,7 @@ class DatabaseManager:
                 image TEXT,
                 used INTEGER DEFAULT 0
             )
-        ''')
+            ''')
 
             conn.execute('''
             CREATE TABLE IF NOT EXISTS winners (
@@ -34,89 +37,111 @@ class DatabaseManager:
                 FOREIGN KEY(user_id) REFERENCES users(user_id),
                 FOREIGN KEY(prize_id) REFERENCES prizes(prize_id)
             )
-        ''')
+            ''')
 
             conn.commit()
 
     def add_user(self, user_id, user_name):
         conn = sqlite3.connect(self.database)
         with conn:
-            conn.execute('INSERT INTO users VALUES (?, ?)', (user_id, user_name))
+            conn.execute('INSERT INTO users (user_id, user_name) VALUES (?, ?)', (user_id, user_name))
             conn.commit()
 
     def add_prize(self, data):
         conn = sqlite3.connect(self.database)
         with conn:
-            conn.executemany('''INSERT INTO prizes (image) VALUES (?)''', data)
+            conn.executemany('INSERT INTO prizes (image) VALUES (?)', data)
             conn.commit()
 
     def add_winner(self, user_id, prize_id):
         win_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         conn = sqlite3.connect(self.database)
         with conn:
-            cur = conn.cursor() 
+            cur = conn.cursor()
             cur.execute("SELECT * FROM winners WHERE user_id = ? AND prize_id = ?", (user_id, prize_id))
             if cur.fetchall():
                 return 0
             else:
-                conn.execute('''INSERT INTO winners (user_id, prize_id, win_time) VALUES (?, ?, ?)''', (user_id, prize_id, win_time))
+                conn.execute('INSERT INTO winners (user_id, prize_id, win_time) VALUES (?, ?, ?)', (user_id, prize_id, win_time))
+                self.add_points(user_id, 10)  # Add points for winning (e.g., 10 points)
                 conn.commit()
                 return 1
 
-  
     def mark_prize_used(self, prize_id):
         conn = sqlite3.connect(self.database)
         with conn:
-            conn.execute('''UPDATE prizes SET used = 1 WHERE prize_id = ?''', (prize_id,))
+            conn.execute('UPDATE prizes SET used = 1 WHERE prize_id = ?', (prize_id,))
             conn.commit()
-
 
     def get_users(self):
         conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
-            cur.execute("SELECT * FROM users ")
-            return [x[0] for x in cur.fetchall()] 
-        
+            cur.execute("SELECT * FROM users")
+            return [x[0] for x in cur.fetchall()]
+
     def get_prize_img(self, prize_id):
         conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
-            cur.execute("SELECT img FROM prizes WHERE prize_id = ?", (prize_id,))
-            return cur.fetchall()[0][0]
+            cur.execute("SELECT image FROM prizes WHERE prize_id = ?", (prize_id,))
+            return cur.fetchone()[0]
 
     def get_random_prize(self):
         conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
-            cur.execute("SELECT * FROM prizes WHERE used = 0 ORDER BY RANDOM()")
-            return cur.fetchall()[0]
-    
+            cur.execute("SELECT * FROM prizes WHERE used = 0 ORDER BY RANDOM() LIMIT 1")
+            return cur.fetchone()
+
     def get_winners_count(self, prize_id):
         conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
-            cur.execute('SELECT COUNT(*) FROM winners WHERE prize_id = ?', (prize_id, ))
-            return cur.fetchall()[0][0]
-      
+            cur.execute('SELECT COUNT(*) FROM winners WHERE prize_id = ?', (prize_id,))
+            return cur.fetchone()[0]
+
     def get_rating(self):
         conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
-            cur.execute('''SELECT users.user_name, COUNT(winners.prize_id) AS count_prize FROM winners 
-                            INNER JOIN users ON users.user_id = winners.user_id GROUP BY winners.user_id ORDER BY count_prize LIMIT 10''')
+            cur.execute('''SELECT users.user_name, COUNT(winners.prize_id) AS count_prize 
+                           FROM winners 
+                           INNER JOIN users ON users.user_id = winners.user_id 
+                           GROUP BY winners.user_id 
+                           ORDER BY count_prize DESC 
+                           LIMIT 10''')
             return cur.fetchall()
-    
+
+    def add_points(self, user_id, points):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('UPDATE users SET points = points + ? WHERE user_id = ?', (points, user_id))
+            conn.commit()
+
+    def get_points(self, user_id):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('SELECT points FROM users WHERE user_id = ?', (user_id,))
+            return cur.fetchone()[0]
+
+    def redeem_points(self, user_id, points):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            current_points = self.get_points(user_id)
+            if current_points >= points:
+                cur.execute('UPDATE users SET points = points - ? WHERE user_id = ?', (points, user_id))
+                conn.commit()
+                return True
+            return False
+
 def hide_img(img_name):
     image = cv2.imread(f'img/{img_name}')
     blurred_image = cv2.GaussianBlur(image, (15, 15), 0)
     pixelated_image = cv2.resize(blurred_image, (30, 30), interpolation=cv2.INTER_NEAREST)
     pixelated_image = cv2.resize(pixelated_image, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_NEAREST)
+    os.makedirs('hidden_img', exist_ok=True)
     cv2.imwrite(f'hidden_img/{img_name}', pixelated_image)
-
-if __name__ == '__main__':
-    manager = DatabaseManager(DATABASE)
-    manager.create_tables()
-    prizes_img = os.listdir('img')
-    data = [(x,) for x in prizes_img]
-    manager.add_prize(data)
